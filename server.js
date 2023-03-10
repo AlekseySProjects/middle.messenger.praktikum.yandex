6dc7a042e0ccdf589e36220f62825d7b943e1c0e
Aleksey S <2766987+AlekseySProjects@users.noreply.github.com>
2023-03-10 20:15:59 +0300

    added lf to eof

--- server.js
+++ server.js
@@ -5,28 +5,12 @@ const path = require('path');
 const router = express.Router();
 const PORT = 3000;
 
-// router.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
-
-// router.get('/auth', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/auth.html'));
-// });
-
-// router.get('/register', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/register.html'));
-// });
-
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/dist'));
 
 router.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-// router.get('*', (req, res) => {
-//   res.status(404).sendFile(path.join(__dirname + '/dist/404.html'));
-// });
 
 app.use('/', router);
 app.listen(PORT);
